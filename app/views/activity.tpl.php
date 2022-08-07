ca4b4d1004255c3b1789216f915b4df22a75a551
francois-michaut <f.michaut@orange.fr>
2022-08-07 16:16:57 +0200

    Mise en place de page activités version mobile et desktop

---
+++ app/views/activity.tpl.php
@@ -0,0 +1,33 @@
+<main class="activity">
+    <h2 class="activity__title">Liste des différentes activités</h2>
+    <div class="activity__table">
+        <table>
+            <thead>
+                <td>Nom</td>
+                <td>Date</td>
+                <td>Lieu</td>
+                <td>Horaires</td>
+            </thead>
+            <tbody>
+                <tr class="first-row row">
+                    <th>Randonnée</th>
+                    <th>22/12/2024</th>
+                    <th>Nice</th>
+                    <th>15h</th>
+                </tr>
+                <tr class="second-row row">
+                    <th>Cinéma</th>
+                    <th>22/12/2024</th>
+                    <th>Nantes</th>
+                    <th>22h</th>
+                </tr>
+                <tr class="first-row row">
+                    <th>Vélo</th>
+                    <th>22/12/2024</th>
+                    <th>Paris</th>
+                    <th>10h</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</main>
--- app/views/header.tpl.php
+++ app/views/header.tpl.php
@@ -5,14 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= $_SERVER['BASE_URI']?>/assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">    
     <title>Back-Office de Family Trip</title>
 </head>
 <body>
     <header class="header">
-        <h1 class="header__title">Back-Office de Family Trip</h1>
+        <h1 class="header__title"><a href="<?= $_SERVER['BASE_URI']?>/">Back-Office de Family Trip</a></h1>
         <nav class="header__navbar">
             <ul class="header__navbar-list">
-                <li><a href="#">Activités</a></li>
+                <li><a href="<?= $_SERVER['BASE_URI']?>/activity">Activités</a></li>
                 <li><a href="#">Shoppinglist</a></li>
                 <li><a href="#">Souvenirs</a></li>
                 <li><a href="#">Tribus</a></li>
