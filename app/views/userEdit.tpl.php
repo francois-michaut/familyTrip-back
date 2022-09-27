952b34aa457f9e1a2151b72c5049c48b6c02d21c
francois-michaut <f.michaut@orange.fr>
2022-09-27 12:40:59 +0200

    userEditPage is ok

--- app/views/userEdit.tpl.php
+++ app/views/userEdit.tpl.php
@@ -2,4 +2,29 @@
     <h2 class="userEdit__title">
         Modifier un utilisateur
     </h2>
+    <form action="" method="post" class="userEdit__form">
+        <div class="input">
+            <label for="firstname">
+                Prénom de l'utilisateur
+            </label>
+            <input type="text" name="firstname" id="firstname" value="<?= $param['user']->getFirstname() ?>" placeholder="<?= $param['user']->getFirstname() ?>">
+        </div>
+        <div class="input">
+            <label for="lastname">
+                Nom de l'utilisateur
+            </label>
+            <input type="text" name="lastname" id="lastname" value="<?= $param['user']->getLastname() ?>" placeholder="<?= $param['user']->getLastname() ?>">
+        </div>
+        <div class="input">
+            <label for="email">
+                Mail de l'utilisateur
+            </label>
+            <input type="email" name="email" id="email" value="<?= $param['user']->getEmail() ?>" placeholder="<?= $param['user']->getEmail() ?>">
+        </div>
+        <div class="userEdit__form--button">
+            <button type="submit">
+                Modifier l'utilisateur
+            </button>
+        </div>
+    </form>
 </main>
